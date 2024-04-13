5a4a8933648802841a3e7cf6b2ef4ad4b19e6a34
Prachi Singh <prachi.singh@simplifyvms.com>
2024-04-14 00:26:04 +0530

    add orgin

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173','https://tool-setup-fe.vercel.app/'],
 };
 
 app.use(cors(corsOptions));
